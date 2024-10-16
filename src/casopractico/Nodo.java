3cb5c797fd2244cdceeb4f91882e5986723c429c
unknown <fabripariachecarrillo.com>
2024-10-16 17:22:10 -0500

    Soy Pariache

---
+++ src/casopractico/Nodo.java
@@ -0,0 +1,16 @@
+package casopractico;
+
+import casopractico.Tarea;
+
+public class Nodo {
+    Tarea tarea;
+    Nodo siguiente;
+
+    public Nodo(Tarea tarea) {
+        this.tarea = tarea;
+        this.siguiente = null;
+    }
+}
+
+    
+
